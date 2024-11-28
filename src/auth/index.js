b52e61701e0849f1ca2b2aa75aa12621ea5fe7f4
Yashasvini Bhanuraj <ybhanuraj@myseneca.ca>
2024-11-28 10:48:55 -0500

    Updated cd.yml

--- src/auth/index.js
+++ src/auth/index.js
@@ -2,10 +2,13 @@
 
 // Prefer Amazon Cognito
 if (process.env.AWS_COGNITO_POOL_ID && process.env.AWS_COGNITO_CLIENT_ID) {
+  console.log('Using Cognito');
   module.exports = require('./cognito');
 }
 // Also allow for an .htpasswd file to be used, but not in production
 else if (process.env.HTPASSWD_FILE && process.NODE_ENV !== 'production') {
+  console.log('Using Basic');
+
   module.exports = require('./basic-auth');
 }
 // In all other cases, we need to stop now and fix our config
